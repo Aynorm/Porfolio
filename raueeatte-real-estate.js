2bbe64f40c9eebf77dad995dd9c621c1e558bd5b
Aynorm <116764902+Aynorm@users.noreply.github.com>
2024-07-27 07:02:19 -0700

    Add files via upload

---
+++ raueeatte-real-estate.js
@@ -0,0 +1,17 @@
+let age = 101
+
+if (age >= 102) {
+   document.getElementById("demo").innerHTML = "you can enter this site"
+} else {
+   document.getElementById("demo").innerHTML = "you cannot enter"
+}
+
+
+let number;
+number = 1;
+
+number++;
+
+document.getElementById("myH1").innerHTML = `My age is ${number}`;
+
+document.getElementById("press");
---
+++ userclear.js
@@ -0,0 +1,11 @@
+//Nav js
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+//Nav js
